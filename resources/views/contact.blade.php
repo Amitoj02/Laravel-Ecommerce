3ac406bdfe0d7cd34fe6008ca642859e5adc4ca0
Amitoj02 <40449197+Amitoj02@users.noreply.github.com>
2024-01-31 02:44:07 -0500

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,53 +10,76 @@
                 <div class="m-lg-5 p-5">
                     <p class="inter text-center">To make an enquire please use our</p>
                     <h1 class="text-primary text-center">Contact Form</h1>
-                    <form class="mt-5 px-lg-5 inter">
+                    <form class="mt-5 px-lg-5 inter" action="#" onsubmit="return submitContactForm()">
                         <div class="row row-cols-1 row-cols-lg-2">
                             <div class="col form-floating my-3">
-                                <input type="text" class="form-control" id="name" placeholder="Name">
-                                <label for="name" class="ms-2">Name</label>
+                                <input type="text" class="form-control" id="firstName" placeholder="Name" @auth value="{{ Auth::user()->name }}" @endauth>
+                                <label for="firstName" class="ms-2">Name</label>
                             </div>
                             <div class="col form-floating my-3">
-                                <input type="text" class="form-control" id="surname" placeholder="Surname">
-                                <label for="surname" class="ms-2">Surname</label>
+                                <input type="text" class="form-control" id="lastName" @auth value="{{ Auth::user()->surname }}" @endauth placeholder="Surname">
+                                <label for="lastName" class="ms-2">Surname</label>
                             </div>
                         </div>
                         <div class="row row-cols-1 row-cols-lg-2">
                             <div class="col form-floating my-3">
-                                <input type="email" class="form-control" id="email" placeholder="Email">
+                                <input type="email" class="form-control" id="email" @auth value="{{ Auth::user()->email }}" @endauth placeholder="Email">
                                 <label for="email" class="ms-2">Email</label>
                             </div>
                             <div class="col form-floating my-3 mb-4">
-                                <input type="text" class="form-control" id="phone" placeholder="Phone">
+                                <input type="text" class="form-control" id="phone" @auth value="{{ Auth::user()->phone_number }}" @endauth placeholder="Phone">
                                 <label for="phone" class="ms-2">Phone</label>
                             </div>
                         </div>
 
                         <b class="text-start">I'm Interested In</b><br />
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox1"
-                                   value="option1">
-                            <label class="form-check-label" for="inlineCheckbox1">Gold Jewellery</label>
+                            <input class="form-check-input" type="checkbox" id="gold" name="interest"
+                                   value="Gold Jewellery">
+                            <label class="gold" for="inlineCheckbox1">Gold Jewellery</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
-                                   value="option2">
-                            <label class="form-check-label" for="inlineCheckbox2">Silver Jewellery</label>
+                            <input class="form-check-input" type="checkbox" id="silver" name="interest"
+                                   value="Silver Jewellery">
+                            <label class="silver" for="inlineCheckbox2">Silver Jewellery</label>
                         </div>
                         <div class="form-check form-check-inline mb-4">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox3"
-                                   value="option3">
-                            <label class="form-check-label" for="inlineCheckbox3">Diamonds</label>
+                            <input class="form-check-input" type="checkbox" id="diamonds" name="interest"
+                                   value="Diamonds">
+                            <label class="diamonds" for="inlineCheckbox3">Diamonds</label>
                         </div>
 
                         <div class="form-floating">
                                             <textarea class="form-control mb-4" placeholder="Leave a message here"
-                                                      id="floatingTextarea2" style="height: 200px" ></textarea>
-                            <label for="floatingTextarea2">Message</label>
+                                                      id="message" name="message" style="height: 200px" ></textarea>
+                            <label for="message">Message</label>
                         </div>
 
                         <input class="btn btn-outline-secondary btn-lg d-grid gap-2 col-6 col-lg-4 mx-auto" type="submit" value="Send Form">
                     </form>
+                    <script>
+                        function submitContactForm() {
+                            var firstName = document.getElementById('firstName').value;
+                            var lastName = document.getElementById('lastName').value;
+                            var email = document.getElementById('email').value;
+                            var phone = document.getElementById('phone').value;
+                            var interests = document.querySelectorAll('input[name="interest"]:checked');
+                            var message = document.getElementById('message').value;
+
+                            // Construct the email body
+                            var emailBody = 'First Name: ' + firstName + ' ' + lastName + '\n'
+                                + 'Email: ' + email + '\n'
+                                + 'Phone: ' + phone + '\n'
+                                + 'Interested in: ' + Array.from(interests).map(i => i.value).join(', ') + '\n'
+                                + 'Message: \n\n' + message;
+
+                            // Redirect user to email client with pre-filled subject and body
+                            window.location.href = 'mailto:info@swarnabhishek.com?subject=Contact Request&body=' + encodeURIComponent(emailBody);
+
+                            // Return false to prevent the form from being submitted in the traditional way
+                            return false;
+                        }
+                    </script>
                 </div>
             </div>
         </div>
