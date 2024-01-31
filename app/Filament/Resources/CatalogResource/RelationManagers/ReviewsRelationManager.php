a2cecf09244e88916bb96f7085dca83af2044d86
Amitoj02 <40449197+Amitoj02@users.noreply.github.com>
2024-01-30 22:48:06 -0500

    Update ReviewsRelationManager.php

--- app/Filament/Resources/CatalogResource/RelationManagers/ReviewsRelationManager.php
+++ app/Filament/Resources/CatalogResource/RelationManagers/ReviewsRelationManager.php
@@ -4,10 +4,14 @@ namespace App\Filament\Resources\CatalogResource\RelationManagers;
 
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
+use IbrahimBougaoua\FilamentRatingStar\Actions\RatingStar;
+use IbrahimBougaoua\FilamentRatingStar\Columns\RatingStarColumn;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ReviewsRelationManager extends RelationManager
@@ -18,16 +22,19 @@ class ReviewsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('title')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('message')
-                    ->maxLength(2000),
                 Forms\Components\Select::make('user_id')
                     ->label('Customer Email')
                     ->relationship(name: 'user', titleAttribute: 'email')
                     ->searchable()
                     ->required()
                     ->live(),
+                RatingStar::make('star')
+                    ->label('Rating'),
+                Forms\Components\TextInput::make('title')
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('message')
+                    ->maxLength(2000),
+
             ]);
     }
 
@@ -36,7 +43,23 @@ class ReviewsRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('title')
             ->columns([
+                Tables\Columns\TextColumn::make('user_id')
+                    ->state(function(Model $record): string{
+                    return $record->user->name.' ('.$record->user->email.')';
+                })
+                ->url(function(Model $record): string{
+                    return env('APP_URL').'/admin/users/'.$record->user_id.'/edit';
+                })
+                ->color('primary')
+                ->icon('heroicon-m-user'),
+
                 Tables\Columns\TextColumn::make('title'),
+                RatingStarColumn::make('star')
+                    ->label('Rating')
+                    ->state(function(Model $record): string{
+                        return (6-$record->star);
+                    }),
+                Tables\Columns\TextColumn::make('message'),
             ])
             ->filters([
                 //
